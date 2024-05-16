1a048156ff3693d574c8ae6ccf490fa666135aa2
BBBtp <167864374+BBBtp@users.noreply.github.com>
2024-05-16 17:37:58 +0300

    Revert "Вертска экрана и разработка логики приложения"

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -23,5 +23,4 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didDiscardSceneSessions sceneSessions: Set<UISceneSession>
     ) {}
-    
 }
